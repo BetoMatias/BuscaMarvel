66a9a1c1d04cc49be071d6258d2e1b4558c2bf12
Shiroi7 <humberto.matiasf@gmail.com>
2020-04-21 23:18:32 -0300

    API request functional

---
+++ apiMarvel.js
@@ -0,0 +1,20 @@
+
+
+var pubkey = "key publica";
+var pvtkey = "key privada";
+var ts = new Date().getTime();
+
+function getCharacter() {
+
+  var hashkey = CryptoJS.MD5(ts + pvtkey + pubkey).toString();
+  var hero = document.getElementById("Name").value;
+  var url = "http://gateway.marvel.com/v1/public/characters?ts=" + ts + "&apikey=" + pubkey + "&hash=" + hashkey + "&name=" + hero;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false);
+  xmlHttp.send(null);
+
+  var character = JSON.parse(xmlHttp.responseText);
+  document.getElementById("heroName").innerHTML = character.data.results[0].name;
+
+}
